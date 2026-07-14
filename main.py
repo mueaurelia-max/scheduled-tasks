f2630f8e5bcaf4c9e2d76c216b6b438c3a899302
mueaurelia-max <mueaurelia@gmail.com>
2026-07-14 06:45:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("catnipkiddy@gmail.com")
+MY_PASSWORD = os.environ.get("rpwntihifrrrkmni")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
